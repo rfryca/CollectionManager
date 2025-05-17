fb01ff8743a23a76f597ab9a172b82d71c36f03c
BoB <robert.fryca@gmail.com>
2025-05-17 17:32:20 +0200

    #2  Add i18n support and integrate Tailwind CSS
    This commit introduces internationalization (i18n) support for Polish localization in the Angular application. It adds new dependencies for Tailwind CSS and tw-elements, along with new components (`MenuComponent` and `TopBarComponent`).
    
    Key changes include:
    - Updated `angular.json` for i18n configurations and build options.
    - Modified `package.json` and `package-lock.json` to include new dependencies.
    - Restructured `AppComponent` to incorporate new components.
    - Updated `main.ts` for Angular localization reference.
    - Adjusted `styles.scss` to utilize Tailwind CSS and tw-elements.
    - Updated TypeScript configuration files for localization types.
    - Created new translation files for English and Polish.
    - Added `.postcssrc.json` for PostCSS configuration.
    - Updated `read.me` with instructions for extracting i18n strings.

--- CollectionManager.FrontEnd/CollectionManager.Angular/src/app/app.component.ts
+++ CollectionManager.FrontEnd/CollectionManager.Angular/src/app/app.component.ts
@@ -1,16 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { Tooltip, initTWE } from 'tw-elements';
+import { Title } from '@angular/platform-browser';  
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, TopBarComponent],
+  // templateUrl: './app.component.html',
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-menu></app-menu>
+    <app-top-bar></app-top-bar>
+    <div class="text-center text-3xl font-bold text-blue-600">
+      {{tailwindText}}
+    </div>
+  
+    <p>
+      Hover the link to see the
+      <a i18n
+        href="#"
+        class="text-primary dark:text-primary-400"
+        data-twe-toggle="tooltip"
+        i18n-title
+        title="It is tooltip text" 
+        >tooltip</a
+      >
+    </p>
     <router-outlet />
   `,
   styles: [],
+  // styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  ngOnInit() {
+    initTWE({ Tooltip }, { allowReinits: true });
+  }
+
   title = 'CollectionManager.Angular';
+  tailwindText=$localize`'Hello Tailwind in Angular!'`;
+  tooltipText=$localize`'This is a tooltip!'`;
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize`${this.title}`);
+  }
 }
+
---
+++ CollectionManager.FrontEnd/CollectionManager.Angular/src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  imports: [],
+  template: `
+    <p>
+      top-bar works!
+    </p>
+  `,
+  styles: ``
+})
+export class TopBarComponent {
+
+}
